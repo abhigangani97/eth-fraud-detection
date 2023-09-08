b0ed599a4c847729200203009ee581e89e35e3ec
abhigangani97 <130812899+abhigangani97@users.noreply.github.com>
2023-09-08 04:51:11 +0100

    Update get_latest_transactions_address.py

--- get_latest_transactions_address.py
+++ get_latest_transactions_address.py
@@ -1,7 +1,23 @@
 import requests
 import json
 from datetime import datetime
+import pickle
+import get_txn_model_input as gtmi
+import pandas as pd
 
+# Load transformer and model from the Pickle files
+with open('transformer_add.pkl', 'rb') as f:
+    transformer_add = pickle.load(f)
+
+with open('model_add.pkl', 'rb') as f:
+    model_add = pickle.load(f)
+
+# Load transformer and model from the Pickle files
+with open('transformer_txn.pkl', 'rb') as g:
+    transformer_txn = pickle.load(g)
+
+with open('model_txn.pkl', 'rb') as g:
+    model_txn = pickle.load(g)
 
 BASE_URL = "https://api.etherscan.io/api"
 API_KEY = "6JXP5MQEZHAU2GE9AUTZGB9Y6STXE632YF"  # Replace with your Etherscan API key
@@ -19,22 +35,37 @@ def get_all_transactions_address(Address):
 
     response = requests.get(BASE_URL, params=params)
     data = response.json()
-    required_keys = ['timestamp', 'hash', 'value','gasPrice']
+    required_keys = ['timestamp', 'hash', 'value', 'gasPrice']
     if data.get("message") == "OK":
         transactions = data["result"]
-        result = [{
-            'timestamp': datetime.utcfromtimestamp(int(d['timeStamp'])).strftime('%Y-%m-%d %H:%M:%S'),
-            'hash': d['hash'],
-            'value': int(d['value']) / 10**18,
-            'gasPrice': int(d['gasPrice']) / 10**18,
-        } for d in transactions[-50:]]
-
         
-        return result
+        # Initialize an empty list to store the transformed data
+        transformed_data = []
+        
+        for d in transactions[-10:]:
+            # Create a dictionary with your data
+            input_data = gtmi.get_transaction_model_input(d['hash'])
+            
+            # Convert the dictionary into a DataFrame
+            input_df = pd.DataFrame([input_data])
+            
+            # Apply the transformer to the DataFrame
+            transformed_df = transformer_txn.transform(input_df)
+            # Add the transformed data along with other information to the list
+            transformed_data.append({
+                'timestamp': datetime.utcfromtimestamp(int(d['timeStamp'])).strftime('%Y-%m-%d %H:%M:%S'),
+                'hash': d['hash'],
+                'value': int(d['value']) / 10**18,
+                'gasPrice': int(d['gasPrice']) / 10**18,
+                'Prediction': model_txn.predict(transformed_df)[0]
+            })
+            print(transformed_data)
+        
+        return transformed_data
     else:
         print(f"Error fetching transactions for address {Address}")
         return []
-        
+
 # Check if the script is being run directly
 if __name__ == '__main__':
     get_latest_transactions_address(Address)
